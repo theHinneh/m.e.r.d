d381f2312f9b87c1c6f4244e1e0751cdc7896ccc
Emmanuel Hinneh <theHinneh@Emmanuels-MacBook-Pro.local>
2021-06-03 22:05:22 +0000

    nginx

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # m.e.r.d
 ## test
+
+>docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
+
+or
+
+>docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d -V
--- index.js
+++ index.js
@@ -33,6 +33,8 @@ mongoose
   .then(() => console.log("db connected"))
   .catch((e) => console.log(e));
 
+app.enable("trust proxy");
+
 app.use(
   session({
     store: new RedisStore({ client: redisClient }),
@@ -49,7 +51,7 @@ app.use(
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api/v1", (req, res) => {
   res.send("<h2>Hi There</h2>");
 });
 
